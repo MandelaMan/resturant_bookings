1a771b726479dc0be869139a852034fd5e011e3f
MandelaMan <nelsonomoro@gmail.com>
2024-05-15 15:19:27 +0300

    updates to menu item

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,5 +1,6 @@
 import logo from "../assets/logo.png"
 import { TfiHome, TfiAgenda , TfiNotepad, TfiBarChartAlt, TfiSettings, TfiLayers , TfiHelpAlt , TfiTruck , TfiGift , TfiWrite  } from "react-icons/tfi";
+import { TbBowlSpoon , TbBrandCakephp , TbToolsKitchen3 } from "react-icons/tb";
 
 const SidebarMenu = () => {
   return (
@@ -12,9 +13,9 @@ const SidebarMenu = () => {
           <li><TfiHome size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Dasboard</li>
           <li><TfiBarChartAlt  size={16} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Reports</li>
           <li><TfiNotepad size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Orders</li>
-          <li><TfiTruck size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Meals</li>
-          <li><TfiAgenda size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Menu</li>
-          <li className="active"><TfiWrite size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Reservations</li>
+          <li><TbBowlSpoon  size={18} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Meals</li>
+          <li><TbBrandCakephp size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Menu</li>
+          <li className="active"><TbToolsKitchen3  size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Reservations</li>
           <li><TfiGift  size={15} style={{ marginBottom: "-0.5%" , fontWeight: 700}}/> Vouchers</li>
           <li><TfiTruck size={15} style={{ marginBottom: "-1%" , fontWeight: 700}}/> Suppliers</li>
         </ul>
